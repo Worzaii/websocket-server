1152ab286a57e388958c22acfb60157620b46e80
Boopkin <boopkin@furnavia.org>
2026-06-19 17:02:38 +0200

    add requestId to req,res

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,7 +1,8 @@
 import { registerCommand } from "./router";
 
-registerCommand("ping", async () => {
+registerCommand("ping", async (command) => {
   return {
+    requestId: command.requestId,
     success: true,
     message: "pong",
   };
--- src/types/command.ts
+++ src/types/command.ts
@@ -1,8 +1,10 @@
 export interface Command {
+  requestId: string;
   action: string;
   payload?: unknown;
 }
 export interface CommandResponse {
+  requestId: string;
   success: boolean;
   message?: string;
   data?: unknown;
