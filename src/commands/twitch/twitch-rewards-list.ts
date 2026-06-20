60f091da8cbe29ebc3df34f9ef84bb61adb6c66a
Boopkin <boopkin@furnavia.org>
2026-06-20 18:02:13 +0200

    prepare new command

---
+++ src/commands/twitch/twitch-rewards-list.ts
@@ -0,0 +1,5 @@
+import { registerCommand } from "../router";
+import { twitchClient } from "../../services/twitch/twitch-client";
+import { Command, CommandResponse } from "../../types/command";
+
+//registerCommand("twitch.rewards.list", handleRewardsList);
